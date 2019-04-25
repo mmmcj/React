b677ef8382fb7ca91616ba354b71956a92f7762b
MadsJuste <Madsjuste89@gmail.com>
2019-04-25 09:38:02 +0200

    opsætning af routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div className="App">
+        <Header/>  
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/user" component={User} />
+        <Route path="/admin" component={Admin} />
+      </div>
+    </Router>    
+  );
+}
+
+function Header(){
+  return( 
+  <ul className="header">
+  <li>
+    <NavLink exact to="/">Home</NavLink>
+  </li>
+  <li>
+    <NavLink to="/user">User</NavLink>
+  </li>
+  <li>
+    <NavLink to="/admin">Admin</NavLink>
+  </li>
+</ul>)
+}
+
+function User(){
+  return (
+    <div>
+      <h2>User</h2>
     </div>
   );
 }
 
+function Admin(){
+  return (
+    <div>
+      <h2>Admin</h2>
+    </div>
+  );
+}
+
+function Home(){
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
 export default App;
