8d28bb70208ea3fead96adf3096dfdd1d3174c01
MadsJuste <Madsjuste89@gmail.com>
2019-04-25 12:43:11 +0200

    minor fiks

--- src/User.js
+++ src/User.js
@@ -4,61 +4,7 @@ import Person from './Person';
 import df from './datafacade';
 
 
-const persons = [
-  {
-    "name": "C-3PO",
-    "height": "167",
-    "mass": "75",
-    "hair_color": "n/a",
-    "skin_color": "gold",
-    "eye_color": "yellow",
-    "birth_year": "112BBY",
-    "gender": "n/a",
-    "homeworld": "https://swapi.co/api/planets/1/",
-    "films": [
-      "https://swapi.co/api/films/2/",
-      "https://swapi.co/api/films/5/",
-      "https://swapi.co/api/films/4/",
-      "https://swapi.co/api/films/6/",
-      "https://swapi.co/api/films/3/",
-      "https://swapi.co/api/films/1/"
-    ],
-    "species": [
-      "https://swapi.co/api/species/2/"
-    ],
-    "vehicles": [],
-    "starships": [],
-    "created": "2014-12-10T15:10:51.357000Z",
-    "edited": "2014-12-20T21:17:50.309000Z",
-    "url": "https://swapi.co/api/people/2/"
-  },
-  {
-    "name": "C-3PO",
-    "height": "167",
-    "mass": "75",
-    "hair_color": "n/a",
-    "skin_color": "gold",
-    "eye_color": "yellow",
-    "birth_year": "112BBY",
-    "gender": "n/a",
-    "homeworld": "https://swapi.co/api/planets/1/",
-    "films": [
-      "https://swapi.co/api/films/2/",
-      "https://swapi.co/api/films/5/",
-      "https://swapi.co/api/films/4/",
-      "https://swapi.co/api/films/6/",
-      "https://swapi.co/api/films/3/",
-      "https://swapi.co/api/films/1/"
-    ],
-    "species": [
-      "https://swapi.co/api/species/2/"
-    ],
-    "vehicles": [],
-    "starships": [],
-    "created": "2014-12-10T15:10:51.357000Z",
-    "edited": "2014-12-20T21:17:50.309000Z",
-    "url": "https://swapi.co/api/people/2/"
-  }];
+const persons = [df.getApi()];
 
 const User = ({ match }) => {
   
