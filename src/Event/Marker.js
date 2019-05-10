356b0d1781b0cc96c33ed36a80d752f54d64f317
JesperPetersenDK <info@ordklar.dk>
2019-05-11 01:58:27 +0200

    Tilføj 2 nye filer

---
+++ src/Event/Marker.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import airlogo from './airportlogo.png';
+
+class Marker extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <img src={airlogo}
+                    height="30rem"
+                    width="20rem"
+                    alt="map marker"
+                    
+                />
+            </div>
+        )
+    }
+}
+
+export default Marker;
---
+++ src/Maps/Maps.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { geolocated, geoPropTypes } from 'react-geolocated';
+import GoogleMapReact from 'google-map-react';
+import Airlogo from '../Event/airportlogo.png';
+import Arrow from '@material-ui/icons/Place';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+
+class Maps extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      coords: [],
+    };
+  }
+
+  static defaultProps = {
+    center: {
+      lat: 50.5,
+      lng: 50.5
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      !this.props.isGeolocationAvailable ? <div>Your browser does not support Geolocation</div>
+        : !this.props.isGeolocationEnabled ? <div>Geolocation is not enabled</div>
+          : this.props.coords ?
+            <div style={{ height: '100vh', width: '100%' }}>
+              <GoogleMapReact
+                defaultCenter={{ lat: this.props.event.city.lattitude, lng: this.props.event.city.longitude }}
+                defaultZoom={this.props.zoom}>
+                <img src={this.props.event.defaultImg}
+                  height="30rem"
+                  width="30rem"
+                  alt="map marker"
+                  lat={this.props.event.city.lattitude}
+                  lng={this.props.event.city.longitude}
+                />
+                <Arrow
+                  lat={this.props.coords.latitude}
+                  lng={this.props.coords.longitude}
+                />         
+
+              </GoogleMapReact>
+            </div>
+            : <div>Getting the location data&hellip; </div>
+    );
+  }
+}
+
+Maps.propTypes = { ...Maps.propTypes, ...geoPropTypes };
+
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(Maps);
